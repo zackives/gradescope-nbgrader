b5f3a99e808b434fa44d666b5cb0fae49b640ef8
Zack Ives <zives@cis.upenn.edu>
2019-08-07 12:12:40 -0400

    A few more packages

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get install -y python3 python3-pip python3-dev
 
-pip3 install nbgrader
+pip3 install nbgrader pandas numpy networkx matplotlib pyspark tensorflow
 # Update these back to the latest versions
 pip3 install jupyter-console ipython
 
