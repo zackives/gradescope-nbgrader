a23040fc6063274a0008cfe43243f0a1843d2d9d
Zack Ives <zives@cis.upenn.edu>
2019-08-07 12:02:40 -0400

    Default config

--- nbgrader_config.py
+++ nbgrader_config.py
@@ -1,3 +1,30 @@
+c = get_config()
+
+###############################################################################
+# Begin additions by nbgrader quickstart
+###############################################################################
+
+# You only need this if you are running nbgrader on a shared
+# server set up.
+c.Exchange.course_id = "cis545"
+
+# Update this list with other assignments you want
+c.CourseDirectory.db_assignments = [dict(name="ps1")]
+
+# Change the students in this list with that actual students in
+# your course
+c.CourseDirectory.db_students = [
+    dict(id="bitdiddle", first_name="Ben", last_name="Bitdiddle"),
+    dict(id="hacker", first_name="Alyssa", last_name="Hacker"),
+    dict(id="reasoner", first_name="Louis", last_name="Reasoner")
+]
+
+c.IncludeHeaderFooter.header = "source/header.ipynb"
+
+###############################################################################
+# End additions by nbgrader quickstart
+###############################################################################
+
 # Configuration file for nbgrader.
 
 #------------------------------------------------------------------------------
@@ -556,7 +583,7 @@
 ## Removes the output from all code cells in a notebook.
 
 ## 
-#c.ClearOutputPreprocessor.remove_metadata_fields = {'scrolled', 'collapsed'}
+#c.ClearOutputPreprocessor.remove_metadata_fields = {'collapsed', 'scrolled'}
 
 #------------------------------------------------------------------------------
 # ClearOutput(NbGraderPreprocessor,ClearOutputPreprocessor) configuration
